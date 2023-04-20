2f303ced5c8a0d41210eae87eb074df4dc72aff4
Juli4nHuber <juliangabrielhr@gmail.com>
2023-04-20 17:37:58 -0300

    Ejercicio 3 exporta

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -1,36 +1,25 @@
 import random
 import time
 
-def anillo_z(n, k):
-    if n < 0:
-        return None
-    elif n < k:
-        return n
-    else:
-        return n % k
-
+# Crear un array aleatorio de 10,000 elementos
+maxNumRandom = 10
+array = random.sample(range(maxNumRandom), 10)
 n = 73
 k = 3
-valor_anillo = anillo_z(n, k)
-print(valor_anillo)
-
-# Crear un array aleatorio de 10,000 elementos
-arr = [random.randint(0, 10000) for i in range(10000)]
 
 # Computar el anillo y medir el tiempo acumulado
 tiempos = []
-tiempo_acumulado = 0
-for i in range(len(arr)):
+tiempoTotal = 0
+for i in range(len(array)):
     inicio = time.time()
-    valor_anillo = anillo_z(arr[i], k)
-    fin = time.time()
-    tiempo_actual = fin - inicio
-    tiempo_acumulado += tiempo_actual
-    tiempos.append(tiempo_acumulado)
+    valorAnillo = n % k
+    tiempo = time.time() - inicio
+    tiempoTotal += tiempo
+    tiempos.append(tiempo)
 
 # Graficar el tiempo acumulado en cada iteración
 import matplotlib.pyplot as plt
-plt.plot(range(len(arr)), tiempos)
+plt.plot(range(len(array)), tiempos)
 plt.xlabel('Número de iteración')
 plt.ylabel('Tiempo acumulado (segundos)')
 plt.show()
